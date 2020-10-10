8917bcbb5f3bad378f0904c27b0bc506baac6eb7
teteumac <43827568+teteumac@users.noreply.github.com>
2020-10-09 22:19:41 -0300

    Create media_combinada_particle.py

---
+++ media_combinada_particle.py
@@ -0,0 +1,23 @@
+## codigo  referente  ao  problema  4 ##
+
+from particle import Particle
+
+Barion_Lambda = Particle.findall('Lambda')[0]
+Barion_Omega_minus = Particle.findall('Omega')[0]
+Lepton_tau_minus = Particle.findall('t')[2]
+Meson_D_plus = Particle.findall('D')[0]
+Meson_B_neutral = Particle.findall('B')[0]
+
+sigma_Barion_Lambda = 0.006
+sigma_Barion_Omega_minus = 0.3
+sigma_Lepton_tau_minus = 0.12
+sigma_Meson_D_plus = 0.05
+sigma_Meson_B_neutral = 0.12
+
+sigma = ( 1 ) / ( (1/sigma_Barion_Lambda**2) + 
+(1/sigma_Barion_Omega_minus**2) + 
+(1/sigma_Lepton_tau_minus**2) + 
+(1/sigma_Meson_D_plus**2) +
+(1/sigma_Meson_B_neutral**2) )
+
+mass_media = ( (sigma/sigma_Barion_Lambda)**2) * Barion_Lambda.mass + ( (sigma/sigma_Barion_Omega_minus)**2) * Barion_Omega_minus.mass + ( (sigma/sigma_Lepton_tau_minus)**2) * Lepton_tau_minus.mass + ( (sigma/sigma_Meson_D_plus)**2) * Meson_D_plus.mass + ( (sigma/sigma_Meson_B_neutral)**2) * Meson_B_neutral.mass 
